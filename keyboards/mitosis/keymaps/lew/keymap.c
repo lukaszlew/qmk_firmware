a29096ed53b24e185979ce1eb256cd4befc2bc73
lew <lew@localhost>
2017-09-30 08:11:49 -0700

    asd

--- keyboards/mitosis/keymaps/lew/keymap.c
+++ keyboards/mitosis/keymaps/lew/keymap.c
@@ -25,13 +25,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {___x___, ___x___, ___x___, ___x___, ___x___,       ___x___, ___x___, ___x___, ___x___, ___x___ },
     {___x___, ___x___, ___x___, KC_LGUI, KC_LCTL,       MKC_SHF, KC_LALT, ___x___, ___x___, ___x___ }
   },
+  // SHIFT
   [L_SHF] = {
-    {_______, _______, _______, _______, _______,       _______, _______, _______, _______, _______ },
-    {_______, _______, _______, _______, _______,       _______, _______, _______, _______, _______ },
-    {MKC_NAV, _______, _______, _______, _______,       _______, _______, KC_SPC , KC_COMM, MKC_SYM },
+    {S(KC_Q), S(KC_W), S(KC_F), S(KC_P), S(KC_B),       S(KC_J), S(KC_L), S(KC_U), S(KC_Y), S(KC_X) },
+    {S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_G),       S(KC_M), S(KC_N), S(KC_E), S(KC_I), S(KC_O) },
+    {MKC_NAV, S(KC_Z), S(KC_C), S(KC_D), S(KC_V),       S(KC_K), S(KC_H), KC_SPC , KC_COMM, MKC_SYM },
     {___x___, ___x___, ___x___, ___x___, ___x___,       ___x___, ___x___, ___x___, ___x___, ___x___ },
     {___x___, ___x___, ___x___, KC_LGUI, KC_LCTL,       MKC_SHF, KC_LALT, ___x___, ___x___, ___x___ }
   },
+  // SYMBOLS
   [L_SYM] = {
     {KC_TILD, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, ___x___, ___x___ },
     {KC_GRV,  KC_COLN, KC_LPRN, KC_RPRN, KC_MINS,       KC_PLUS, KC_DQUO, KC_EXLM, KC_QUES, ___x___ },
@@ -39,6 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {___x___, ___x___, ___x___, ___x___, ___x___,       ___x___, ___x___, ___x___, ___x___, ___x___ },
     {___x___, ___x___, ___x___, KC_LGUI, KC_LCTL,       MKC_SHF, KC_LALT, ___x___, ___x___, ___x___ }
   },
+  // SYMBOL SHIFT
   [L_SYM_SHF] = {
     {___x___, ___x___, KC_LABK, KC_RABK, ___x___,       KC_DOT , KC_7   , KC_8   , KC_9   , ___x___ },
     {___x___, ___x___, KC_LCBR, KC_RCBR, KC_PIPE,       KC_PLUS, KC_4   , KC_5   , KC_6   , ___x___ },
@@ -49,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_NAV] = {
     {___x___, KC_PSCR, KC_ESC , ___x___, KC_VOLU,       KC_MENU, KC_HOME, KC_UP  , KC_END , KC_INS  },
     {___x___, ___x___, ___x___, KC_TAB , KC_VOLD,       KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, ___x___ }, // TODO 5x acceleration
-    {MKC_NAV, ___x___, KC_WBAK, KC_WFWD, KC_MUTE,       KC_PGDN, KC_BSPC, KC_ENT , KC_COMM, MKC_SYM }, // TODO more media buttons
+    {MKC_NAV, ___x___, KC_WBAK, KC_WFWD, KC_MUTE,       KC_PGDN, KC_BSPC, KC_ENT , KC_DEL , MKC_SYM }, // TODO more media buttons
     {___x___, ___x___, ___x___, ___x___, ___x___,       ___x___, ___x___, ___x___, ___x___, ___x___ },
     {___x___, ___x___, ___x___, KC_LGUI, KC_LCTL,       MKC_SHF, KC_LALT, ___x___, ___x___, ___x___ }
   },
@@ -70,39 +73,43 @@ void update_layer_state(enum layers layer, uint32_t is_on) {
   }
 }
 
-static uint8_t shf_pressed = 0;
-static uint8_t sym_pressed = 0;
-static uint8_t nav_pressed = 0;
+static bool shf_pressed = 0;
+static bool sym_pressed = 0;
+static bool nav_pressed = 0;
+
+
 
 void update_all_layers(void) {
   update_layer_state(L_BASE, true);
-  update_layer_state(L_SYM, sym_pressed);
-  update_layer_state(L_NAV, nav_pressed);
   update_layer_state(L_SHF, shf_pressed);
+  update_layer_state(L_SYM, sym_pressed);
   update_layer_state(L_SYM_SHF, sym_pressed && shf_pressed);
+  update_layer_state(L_NAV, nav_pressed);
   update_layer_state(L_SYM_NAV, sym_pressed && nav_pressed);
+  // Apply LSFT uniformly to nav layers instead of using S() macro
+  if (nav_pressed) {
+    if (shf_pressed) {
+      register_code(KC_LSFT);
+    } else {
+      unregister_code(KC_LSFT);
+    }
+  }
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
   case MKC_SYM:
     sym_pressed = record->event.pressed;
-    update_all_layers();
-    return false;
+    break;
   case MKC_NAV:
     nav_pressed = record->event.pressed;
-    update_all_layers();
-    return false;
+    break;
   case MKC_SHF:
     shf_pressed = record->event.pressed;
-    if (record->event.pressed) {
-      register_code(KC_LSFT);
-    } else {
-      unregister_code(KC_LSFT);
-    }
-    update_all_layers();
-    return false;
+    break;
   default:
     return true;
   }
+  update_all_layers();
+  return false;
 }
